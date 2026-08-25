a6811f89c97f5517b0317741be13bad976b447f0
Stringless Puppet <Jlumapas1334@gmail.com>
2026-08-25 20:49:50 +0800

    changes added

--- src/EquiBorrow.Infrastructure/Repositories/MemoryStudentRepos.cs
+++ src/EquiBorrow.Infrastructure/Repositories/MemoryStudentRepos.cs
@@ -12,7 +12,14 @@ public class InMemoryStudentRepository : IStudentRepository
     {
         [1] = new Student(1, "Ash", true),
         [2] = new Student(2, "Billy Jean", false),
-        [3] = new Student(3, "Charlie Wonka", true)
+        [3] = new Student(3, "Charlie Puth", true),
+        [4] = new Student(4, "Ashley", true),
+        [5] = new Student(5, "Steven", true),
+        [6] = new Student(6, "Joanna", false),
+        [7] = new Student(7, "Fluffy", false),
+        [8] = new Student(8, "Quifrey", true),
+        [9] = new Student(9, "Alice", true),
+        [10] = new Student(10, "Bob", false),
     };
 
     public Task<Student?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
